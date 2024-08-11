4b283e20115ab93a1246aa3b467a9d7638dffff1
s-tsarkov <carkov1990@gmail.com>
2024-08-11 17:02:55 +0400

    add comment

--- fahr.c
+++ fahr.c
@@ -1,22 +1,24 @@
 #include <stdio.h>
-main(int argc, int lower, int upper, int klz, char *argv[])
+//fahr.out -l 0 -u 100
+//где -l это lower
+//-u это upper
+int main(int argc, int lower, int upper, int klz, char *argv[])
 {
-printf("Input your lower:");
-scanf("%i", &lower);
+  printf("Input your lower:");
+  scanf("%i", &lower);
 
-printf("Input you upper:");
-scanf("%i", &upper);
+  printf("Input you upper:");
+  scanf("%i", &upper);
 
-    {
-        int fahr, step;
-
-    for(fahr = lower; fahr <= 300; fahr = 5)
+    
+  int fahr, step;
+  for(fahr = lower; fahr <= upper; fahr = fahr + 5)
+  {
     printf("%3d %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
-    }
+  }
 
-
-printf("Press number 1 and Enter");
-scanf("%i", &klz);
+  printf("Press number 1 and Enter");
+  scanf("%i", &klz);
 }
 
 
