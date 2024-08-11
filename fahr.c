b0285410f55afe24122a8be7a979a68c2b6af27d
e-tsarkow <egorcarkow@gmail.com>
2024-08-11 16:35:23 +0400

    change build

---
+++ fahr.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+main(int argc, int lower, int upper, int klz, char *argv[])
+{
+printf("Input your lower:");
+scanf("%i", &lower);
+
+printf("Input you upper:");
+scanf("%i", &upper);
+
+
+
+    {
+        int fahr, step;
+
+    for( int 0 = lower; fahr <= 300; step = 5)
+    printf("%3d %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
+    }
+
+
+printf("Press number 1 and Enter");
+scanf("%i", &klz);
+}
+
+
+
